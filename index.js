416d564d6c9900d1142ff2d5121a36a54fa3f074
Aswitha-Ajin <1112aswitha@gmail.com>
2022-12-23 13:03:12 +0530

    cors

--- index.js
+++ index.js
@@ -12,9 +12,17 @@ const app = express();
 // to parse req.body, to send the req.body from client to server
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://zingy-biscotti-fcd882.netlify.app"
-}))
+// app.use(cors({
+//   origin: "https://zingy-biscotti-fcd882.netlify.app"
+// }))
+app.use(cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 // app.use(cors({
 //   origin: "http://localhost:3000/",
 // }))
